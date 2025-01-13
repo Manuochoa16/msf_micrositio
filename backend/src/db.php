0e969dd526a2699cac09d3147dbfcc5f2dfbca43
EJScalerandi <estebanscalerandi.dev@gmail.com>
2025-01-13 15:08:49 -0300

    Cambios conexion a base de datos

--- backend/src/db.php
+++ backend/src/db.php
@@ -1,20 +1,20 @@
 <?php
-// Ya no es necesario cargar phpdotenv, ya que las variables de entorno están definidas en Render
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Extraer los componentes de la URL directamente desde las variables de entorno
-$host = getenv('MYSQL_HOST'); // autorack.proxy.rlwy.net
-$dbname = getenv('MYSQL_DATABASE'); // railway
-$username = getenv('MYSQL_USER'); // root
-$password = getenv('MYSQL_ROOT_PASSWORD'); // fyAHXQKUbPNyXZLwPWjdsbNKfvTEosqU
-$port = getenv('MYSQL_PORT'); // 55045
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '\..\manualdemarca.env');
+$dotenv->load();
+
+$host = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DATABASE');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$port = getenv('MYSQL_PORT');
 
 try {
-    // Crear la conexión a la base de datos
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   
 } catch (PDOException $e) {
     die(json_encode(['error' => 'Database connection failed', 'message' => $e->getMessage()]));
 }
-?>
+
